1f671910b43a2cfbbeaee41e012ae0ea08a2a33d
Ayush Kiledar <88903550+Ayush-2019@users.noreply.github.com>
2022-07-08 19:33:36 +0530

    Create Building max heap from array.java

---
+++ Heap/Building max heap from array.java
@@ -0,0 +1,32 @@
+class Solution {
+  
+    static void heapify(int arr[], int n, int i)
+    {
+        int largest = i; 
+        int l = 2 * i + 1; 
+        int r = 2 * i + 2; 
+ 
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+  
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+  
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+  
+            heapify(arr, n, largest);
+        }
+    }
+  
+    static void buildHeap(int arr[], int n)
+    {
+        int startIdx = (n / 2) - 1;
+  
+        for (int i = startIdx; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+    }  
+}
