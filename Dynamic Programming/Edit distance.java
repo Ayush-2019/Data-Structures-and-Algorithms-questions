09a1b9603e4626cb6d8bec9ccaebcb8b2fb99206
Ayush Kiledar <88903550+Ayush-2019@users.noreply.github.com>
2022-07-24 11:42:44 +0530

    Create Edit distance.java
    Given two strings s and t. Return the minimum number of operations required to convert s to t.
    The possible operations are permitted:
    
    Insert a character at any position of the string.
    Remove any character from the string.
    Replace any character from the string with any other character.
     
    
    Example 1:
    
    Input: 
    s = "geek", t = "gesek"
    Output: 1
    Explanation: One operation is required 
    inserting 's' between two 'e's of str1.
    Example 2:
    
    Input : 
    s = "gfg", t = "gfg"
    Output: 
    0
    Explanation: Both strings are same.
     
    
    Your Task:
    You don't need to read or print anything. Your task is to complete the function editDistance() which takes strings s and t as input parameters and returns the minimum number of operation to convert the string s to string t. 
    
    
    Expected Time Complexity: O(|s|*|t|)
    Expected Space Complexity: O(|s|*|t|)
    
    
    Constraints:
    1 ≤ Length of both strings ≤ 100
    Both the strings are in lowercase.

---
+++ Dynamic Programming/Edit distance.java
@@ -0,0 +1,29 @@
+class Solution {
+    int solve(int x,int y,String s,String t,int dp[][]){
+        if(x == -1) return y+1;
+        if(y == -1) return x+1;
+        
+        if(dp[x][y]!=-1) return dp[x][y];
+        
+        if(s.charAt(x) == t.charAt(y)) return dp[x][y] = solve(x-1,y-1,s,t,dp);
+        
+        int a = solve(x-1,y-1,s,t,dp);
+        int b = solve(x,y-1,s,t,dp);
+        int c = solve(x-1,y,s,t,dp);
+        
+        return dp[x][y] = 1 + Math.min(a,Math.min(b,c));
+    }
+    public int editDistance(String s, String t) {
+        // Code here
+        int x = s.length(), y = t.length();
+        
+        int dp[][] = new int[x][y];
+        for(int i=0;i<x;i++){
+            for(int j=0;j<y;j++){
+                dp[i][j] = -1;
+            }
+        }
+        
+        return solve(x-1,y-1,s,t,dp);
+    }
+}
