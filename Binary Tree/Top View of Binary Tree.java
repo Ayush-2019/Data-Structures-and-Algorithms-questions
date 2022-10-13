74b9c412a253d684e18e08b51b778e4fc2c3c454
Ayush Kiledar <88903550+Ayush-2019@users.noreply.github.com>
2022-10-13 22:44:38 +0530

    Create Top View of Binary Tree.java
    Given below is a binary tree. The task is to print the top view of binary tree. Top view of a binary tree is the set of nodes visible when the tree is viewed from the top. For the given below tree
    
           1
        /     \
       2       3
      /  \    /   \
    4    5  6   7
    
    Top view will be: 4 2 1 3 7
    Note: Return nodes from leftmost node to rightmost node.
    
    Example 1:
    
    Input:
          1
       /    \
      2      3
    Output: 2 1 3
    Example 2:
    
    Input:
           10
        /      \
      20        30
     /   \    /    \
    40   60  90    100
    Output: 40 20 10 30 100
    Your Task:
    Since this is a function problem. You don't have to take input. Just complete the function topView() that takes root node as parameter and returns a list of nodes visible from the top view from left to right.
    
    Expected Time Complexity: O(NlogN)
    Expected Auxiliary Space: O(N).
    
    Constraints:
    1 ≤ N ≤ 105
    1 ≤ Node Data ≤ 105

---
+++ Binary Tree/Top View of Binary Tree.java
@@ -0,0 +1,58 @@
+class Solution
+{
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+        static void levelOrder(Node node , TreeMap<Integer , Integer> map){
+        
+        //Queue to store the node for level order
+        Queue<Node> q1 = new LinkedList<Node>();
+        
+        //Queue to store vertical level value. ie. n = node , left = n-1 & right = n+1
+        Queue<Integer> q2 = new LinkedList<Integer>();
+        
+        q1.add(node);
+        q2.add(0);
+        
+        while( (!q1.isEmpty()) && (!q2.isEmpty()) ){
+            
+            Node temp = q1.peek();
+            int temp1 = q2.peek();
+            q1.remove() ;
+            q2.remove();
+            
+            //cheking if it is already stored at particular index (ie. upper node), no need to store since it needs upper node
+            if(!map.containsKey(temp1))
+                map.put(temp1 , temp.data);
+            
+            if(temp.left != null){
+                q1.add(temp.left);
+                q2.add(temp1-1);
+            }    
+            
+            if(temp.right != null){
+                q1.add(temp.right);
+                q2.add(temp1 + 1);
+            }
+        }
+        
+        return;  
+    }
+    
+    // function should print the topView of the binary tree
+    static ArrayList<Integer> topView(Node root)
+    {
+    // Code here
+        //since TreeMap is same as HashMap and also it stores value in ascending order , therefore we dont need to sort it.
+        TreeMap<Integer , Integer> map = new TreeMap<Integer , Integer>();
+        
+        levelOrder(root , map);
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        for(int x: map.keySet())
+            ans.add(map.get(x));
+            
+        return ans;
+        
+        
+    }
+        
+    }
