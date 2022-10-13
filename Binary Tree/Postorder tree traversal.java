c05b8bf5506717de0e9e2c65475fe41a97cb7052
Ayush Kiledar <88903550+Ayush-2019@users.noreply.github.com>
2022-10-13 18:28:14 +0530

    Create Postorder tree traversal.java

---
+++ Binary Tree/Postorder tree traversal.java
@@ -0,0 +1,45 @@
+class GFG
+{
+    public static void postorderRec(Node root)
+    {
+        if (root == null) {
+            return;
+        }
+    
+        postorder(root.left);
+    
+        postorder(root.right);
+    
+        System.out.print(root.data + " ");
+    }
+ 
+    public static void postorderIterative(Node root)
+    {
+        if (root == null) {
+            return;
+        }
+ 
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+    
+        Stack<Integer> out = new Stack<>();
+    
+        while (!stack.empty())
+        {
+            Node curr = stack.pop();
+            out.push(curr.data);
+    
+            if (curr.left != null) {
+                stack.push(curr.left);
+            }
+    
+            if (curr.right != null) {
+                stack.push(curr.right);
+            }
+        }
+    
+        while (!out.empty()) {
+            System.out.print(out.pop() + " ");
+        }
+    }
+}
