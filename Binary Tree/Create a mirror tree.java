993aec557e7cbe85ab8a9a3bb6b8b674697ed3d4
Ayush Kiledar <88903550+Ayush-2019@users.noreply.github.com>
2022-10-13 18:07:23 +0530

    Create Create a mirror tree.java
    Given a binary tree, the task is to create a new binary tree which is a mirror image of the given binary tree.
    
    Examples: 
    
    Input:
            5
           / \
          3   6
         / \
        2   4
    Output:
    Inorder of original tree: 2 3 4 5 6 
    Inorder of mirror tree: 6 5 4 3 2
    Mirror tree will be:
      5
     / \
    6   3
       / \
      4   2
    
    Input:
            2
           / \
          1   8
         /     \
        12      9
    Output:
    Inorder of original tree: 12 1 2 8 9 
    Inorder of mirror tree: 9 8 2 1 12

---
+++ Binary Tree/Create a mirror tree.java
@@ -0,0 +1,16 @@
+class GFG{
+
+public static Node mirrorTree(Node root)
+{
+	if (root == null)
+		return null;
+
+	Node left = mirrorTree(root.left);
+	Node right = mirrorTree(root.right);
+
+	root.left = right;
+	root.right = left;
+
+	return root;
+}
+}
