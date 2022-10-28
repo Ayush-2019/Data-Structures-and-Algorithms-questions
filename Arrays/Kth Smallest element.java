62f3f25d177b9fe51ac5a5a1bb73d2c207632351
Ayush Kiledar <88903550+Ayush-2019@users.noreply.github.com>
2022-10-28 20:58:15 +0530

    Create Kth Smallest element.java
    Given an array arr[] and an integer K where K is smaller than size of array, the task is to find the Kth smallest element in the given array. It is given that all array elements are distinct.
    
    Example 1:
    
    Input:
    N = 6
    arr[] = 7 10 4 3 20 15
    K = 3
    Output : 7
    Explanation :
    3rd smallest element in the given 
    array is 7.
    Example 2:
    
    Input:
    N = 5
    arr[] = 7 10 4 20 15
    K = 4
    Output : 15
    Explanation :
    4th smallest element in the given 
    array is 15.
    Your Task:
    You don't have to read input or print anything. Your task is to complete the function kthSmallest() which takes the array arr[], integers l and r denoting the starting and ending index of the array and an integer K as input and returns the Kth smallest element.
     
     
    Expected Time Complexity: O(n)
    Expected Auxiliary Space: O(log(n))
    Constraints:
    1 <= N <= 105
    1 <= arr[i] <= 105
    1 <= K <= N

---
+++ Arrays/Kth Smallest element.java
@@ -0,0 +1,41 @@
+//User function Template for Java
+
+class Solution{
+    
+    public static int part(int arr[], int low, int high){
+        
+        int pivot = arr[high];
+        
+        int i = low-1;
+        
+        for(int j=low;j<high;j++){
+            
+            if(arr[j]<pivot){
+               
+               i++;
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        int temp = arr[i+1];
+                arr[i+1] = arr[high];
+                arr[high] = temp;
+                
+                return i+1;
+    }
+    
+    public static int kthSmallest(int[] arr, int l, int r, int k) 
+    { 
+        //Your code here
+        int partition = part(arr,l,r);
+        
+        if(partition == k-1) return arr[partition];
+        
+        else if(partition<k-1) return kthSmallest(arr, partition+1,r,k);
+        
+        else return kthSmallest(arr,l,partition-1,k);
+        
+        
+    } 
+}
