c6fab17e43a4bfdf0376a842cb18fdc1e14092fd
Ayush Kiledar <88903550+Ayush-2019@users.noreply.github.com>
2022-11-03 21:15:00 +0530

    Create Common elements in all rows of a given matrix.java
    Given an m x n matrix, find all common elements present in all rows in O(mn) time and one traversal of matrix.
    
    Example: 
    
    Input:
    mat[4][5] = {{1, 2, 1, 4, 8},
                 {3, 7, 8, 5, 1},
                 {8, 7, 7, 3, 1},
                 {8, 1, 2, 7, 9},
                };
    
    Output: 
    1 8 or 8 1
    8 and 1 are present in all rows.
    
    Time Complexity: O(m * n)
    Auxiliary Space: O(N)

---
+++ Matrix/Common elements in all rows of a given matrix.java
@@ -0,0 +1,26 @@
+class GFG
+{
+static void printCommonElements(int mat[][], int N, int M)
+{
+ 
+    Map<Integer,Integer> mp = new HashMap<>();
+     
+    for (int j = 0; j < N; j++)
+        mp.put(mat[0][j],1);
+         
+    for (int i = 1; i < M; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+
+            if (mp.get(mat[i][j]) != null && mp.get(mat[i][j]) == i)
+            {
+                mp.put(mat[i][j], i + 1);
+ 
+                if (i == M - 1)
+                    System.out.print(mat[i][j] + " ");
+            }
+        }
+    }
+}
+}
