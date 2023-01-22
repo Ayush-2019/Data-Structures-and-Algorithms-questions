46c63b030eb6bd381b3c1d2595e5f752d91d6dd4
Ayush Kiledar <88903550+Ayush-2019@users.noreply.github.com>
2023-01-22 13:04:27 +0530

    Update Kth Ancestor in a Tree.java
    Given a binary tree of size  N, a node, and a positive integer k., Your task is to complete the function kthAncestor(), the function should return the kth ancestor of the given node in the binary tree. If there does not exist any such ancestor then return -1.
    Note: It is guaranteed that the node exists in the tree.
    
    
    Example 1:
    
    
    
    Input:
         K = 2
         Node = 4
    Output: 1
    Explanation:
    Since, K is 2 and node is 4, so we
    first need to locate the node and
    look k times its ancestors.
    Here in this Case node 4 has 1 as his
    2nd Ancestor aka the Root of the tree.
     
    
    Example 2:
    
    Input:
    k=1 
    node=3
          1
        /   \
        2     3
    
    Output:
    1
    Explanation:
    K=1 and node=3 ,Kth ancestor of node 3 is 1.
    
    Expected Time Complexity: O(N)
    Expected Auxiliary Space: O(N)
    
     
    
    Constraints:
    1<=N<=104
    1<= K <= 100

--- Binary Tree/Kth Ancestor in a Tree.java
+++ Binary Tree/Kth Ancestor in a Tree.java
@@ -19,6 +19,34 @@ class Tree
         list.remove(list.size()-1);
     }
     
+    //eff. approach O(1) space
+    static Node kthAncestorDFS(Node root, int node )
+{
+    if (root == null)
+        return null;
+     
+    if (root.data == node||
+    (temp = kthAncestorDFS(root.left,node)) != null ||
+    (temp = kthAncestorDFS(root.right,node)) != null)
+    {
+        if (k > 0)    
+            k--;
+         
+        else if (k == 0)
+        {
+            System.out.print("Kth ancestor is: "+root.data);
+             
+            
+            return null;
+        }
+         
+        return root;
+    }
+    return null;
+        
+        //if (kthAncestorDFS(root,node) != null) System.out.println("-1");    Put this code in main function
+}
+    
     public int kthAncestor(Node root, int k, int node)
     {
         //Write your code here
